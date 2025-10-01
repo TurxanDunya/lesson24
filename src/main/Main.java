0592d3b173b9e58101fa6fae584482ec9acc98be
Turxan Dunyamaliyev <50988453+TurxanDunya@users.noreply.github.com>
2025-10-01 20:38:27 +0400

    last review notes fixed

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,19 +6,21 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
 
-        int i = 0;
-        while (true) {
-            System.out.println(++i);
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
+        for (int i = 0; i < 10; i++) {
 
-            if (i == 3) {
-                break;
-            }
         }
+//        while (true) {
+//            System.out.println(++i);
+//            try {
+//                Thread.sleep(1000);
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+//
+//            if (i == 3) {
+//                break;
+//            }
+//        }
 
         System.out.println("End of thread");
     }
